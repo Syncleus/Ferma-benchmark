ef3c55d9a36a714e666e250157bd0d374918119d
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2014-11-27 17:31:16 -0500

    Initial commit, includes a basic set of starting points for benchmarks.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ferma Benchmarks
+
+This is a simple application to act as a benchmark comparing the Ferma Graph-to-Object layer with Totorom, TinkerPop
+Frames, and TinkerPop Blueprints. It will help us ensure Ferma remains the top-performing option.
+
+For more information see the main [Ferma Project page](https://github.com/freemo/Ferma-benchmark).
+
+## Obtaining the Source
+
+The official source repository for Ferma Benchmarks is located on the Syncleus Gerrit instance and can be cloned using the
+following command.
+
+```
+git clone http://gerrit.syncleus.com/Ferma-benchmark
+```
+
+We also maintain a GitHub clone of the official repository which can be found
+[here](https://github.com/Syncleus/Ferma-benchmark). Finally Syncleus also hosts an instance of GitLab which has a
+clone of the repository which can be found [here](http://gitlab.syncleus.com/syncleus/Ferma-benchmark).
---
+++ src/main/java/com/syncleus/ferma/benchmark/God.java
@@ -0,0 +1,105 @@
+/******************************************************************************
+ *                                                                             *
+ *  Copyright: (c) Syncleus, Inc.                                              *
+ *                                                                             *
+ *  You may redistribute and modify this source code under the terms and       *
+ *  conditions of the Open Source Community License - Type C version 1.0       *
+ *  or any later version as published by Syncleus, Inc. at www.syncleus.com.   *
+ *  There should be a copy of the license included with this file. If a copy   *
+ *  of the license is not included you are granted no right to distribute or   *
+ *  otherwise use this file except through a legal and valid license. You      *
+ *  should also contact Syncleus, Inc. at the information below if you cannot  *
+ *  find a license:                                                            *
+ *                                                                             *
+ *  Syncleus, Inc.                                                             *
+ *  2604 South 12th Street                                                     *
+ *  Philadelphia, PA 19148                                                     *
+ *                                                                             *
+ ******************************************************************************/
+package com.syncleus.ferma.benchmark;
+
+import com.syncleus.ferma.FramedEdge;
+import com.syncleus.ferma.FramedVertex;
+import com.syncleus.ferma.annotations.Adjacency;
+import com.syncleus.ferma.annotations.Incidence;
+import com.syncleus.ferma.annotations.Property;
+import com.tinkerpop.blueprints.Direction;
+
+public interface God {
+    /*
+        @Adjacency(label="father")
+        God getFather();
+
+        @Adjacency(label="father", direction= Direction.IN)
+        Iterable<? extends God> getSons();
+
+        @Adjacency(label="lives")
+        Location getHome();
+    */
+
+    @Property("name")
+    String getName();
+
+    @Property("name")
+    void setName(String newName);
+
+    @Property("name")
+    void removeName();
+
+    @Property("age")
+    Integer getAge();
+
+    @Property("type")
+    String getType();
+
+    @Adjacency(label="father", direction= Direction.IN)
+    Iterable<? extends God> getSons();
+
+    @Adjacency(label="father", direction= Direction.IN)
+    God getSon();
+
+    @Adjacency(label="father", direction= Direction.IN)
+    <N extends God> Iterable<? extends N> getSons(Class<? extends N> type);
+
+    @Adjacency(label="father", direction= Direction.OUT)
+    <N extends God> Iterable<? extends N> getParents();
+
+    @Adjacency(label="father", direction= Direction.IN)
+    <N extends God> N getSon(Class<? extends N> type);
+
+    @Adjacency(label="father", direction= Direction.IN)
+    <N extends God> N addSon(Class<? extends N> type);
+
+    @Adjacency(label="father", direction= Direction.IN)
+    <N extends God> N addSon(Class<? extends N> type, Class<? extends FatherEdge> edge);
+
+    @Adjacency(label="father", direction= Direction.IN)
+    God addSon(God son);
+
+    @Adjacency(label="father", direction= Direction.IN)
+    FramedVertex addSon();
+
+    @Adjacency(label="father", direction= Direction.IN)
+    God addSon(God son, Class<? extends FatherEdge> edge);
+
+    @Adjacency(label="father", direction= Direction.IN)
+    void setSons(Iterable<? extends God> vertexSet);
+
+    @Adjacency(label="father", direction= Direction.IN)
+    void removeSon(God son);
+
+    @Incidence(label="father", direction= Direction.IN)
+    Iterable<? extends FramedEdge> getSonEdges();
+
+    @Incidence(label="father", direction= Direction.IN)
+    <N extends FatherEdge> Iterable<? extends N> getSonEdges(Class<? extends N> type);
+
+    @Incidence(label="father", direction= Direction.IN)
+    FramedEdge getSonEdge();
+
+    @Incidence(label="father", direction= Direction.IN)
+    <N extends FatherEdge> N getSonEdge(Class<? extends N> type);
+
+    @Incidence(label="father", direction= Direction.IN)
+    void removeSonEdge(FatherEdge edge);
+}
